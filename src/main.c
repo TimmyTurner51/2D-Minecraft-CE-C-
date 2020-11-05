3ef9d63060dbee03f1f4497b94ccc754e1d47103
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-11-05 13:55:02 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,18 +5,32 @@
 //  Description: A 2D Minecraft clone made for the TI-84 Plus CE, written in C.
 ////////////////////////////////////////////////////////////////////////////////
 
-[i]OPEN THE APPVAR
-64->NSPRITES
-16*16+2->SPRITESIZE
-Open(Str0,"r"->D
-GetDataPtr(D->PTR
-Close(D
-NSPRITES*3->N
-Alloc(N->SPRITES
-0->A
-Repeat A=N
-	PTR->{SPRITES+A
-	PTR+SPRITESIZE->PTR
-	A+3->A
-End
-
+void LoadBlocks(void);
+	uint24_t numSprites;
+	uint24_t spriteSize;
+	uint24_t sprites;
+	uint24_t Pointer;
+	uint24_t data;
+	ti_var_t appvar
+	
+	
+	
+	
+	
+	
+	
+	void LoadBlocks() {
+	numSprites = 64;
+	spriteSize = (16 * 16) + 2;
+	appvar = ti_Open("pack", "r");
+	Pointer = ti_GetDataPtr(appvar);
+	ti_CloseAll;
+	data = numSprites * 3;
+	sprites = ti_mAlloc(N);
+	x = 0;
+	While (x = data) {
+		{SPRITES+A} = Pointer;
+		Pointer = Pointer + spriteSize;
+		x += 3;
+		}
+}
