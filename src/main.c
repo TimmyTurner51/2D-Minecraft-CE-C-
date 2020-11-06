76b119c497d5a87e549562548c9fbc7de0fb4174
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-11-06 11:10:38 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 ////////////////////////////////////////////////////////////////////////////////
 //  2D Minecraft CE (C)
 //  Authors: TimmyTurner51, LogicalJoe, and Beckadamtheinventor
@@ -5,32 +6,58 @@
 //  Description: A 2D Minecraft clone made for the TI-84 Plus CE, written in C.
 ////////////////////////////////////////////////////////////////////////////////
 
+	static uint24_t numSprites;
+	static uint24_t sprites;
+  
+void main(void);
 void LoadBlocks(void);
-	uint24_t numSprites;
-	uint24_t spriteSize;
-	uint24_t sprites;
-	uint24_t Pointer;
-	uint24_t data;
-	ti_var_t appvar;
-	
-	
-	
-	
-	
-	
-	
-	void LoadBlocks() {
+
+//first, run LoadBlocks(), then, draw the main menu...
+
+void main(void) {
+	gfx_Begin();
+	gfx_SetPalette(myPalette, sizeof_myPalette, 0);
+  LoadBlocks();
+  DrawMenu();
+  
+}
+
+void DrawMenu(void) {
+	int x, y, CursorY;
+	gfx_SetDrawBuffer();
+  for (x = 0; x < 20; x++) {
+  	for (y = 0; y < 15; y++) {
+    	gfx_SpriteNoClip(dirt, x * 16, y * 16);
+    }
+  }
+  
+	while !(kb_IsDown(kb_Key2nd)) {  
+  	kb_Scan();
+  	
+  	
+    gfx_BlitBuffer();
+  }
+  
+}
+
+					will this site thing stay up for more than a day, or a session, or whatever?
+
+void LoadBlocks(void) {
+	int x, spriteSize;
+  ti_var_t appvar;
+  
 	numSprites = 64;
 	spriteSize = (16 * 16) + 2;
 	appvar = ti_Open("pack", "r");
 	Pointer = ti_GetDataPtr(appvar);
-	ti_CloseAll;
+	ti_CloseAll();
 	data = numSprites * 3;
 	sprites = ti_mAlloc(N);
-	x = 0;
-	While (x = data) {
-		{sprites + x} = Pointer;
-		Pointer = Pointer + spriteSize;
+	for (x = 0) {
+		sprites[x] = Pointer;
+		Pointer += spriteSize;
 		x += 3;
 		}
+
 }
+
