db902e497ba96e6488bd45698dd238df76913cd8
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-11-13 13:01:55 -0600

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,57 +6,80 @@
 //  Description: A 2D Minecraft clone made for the TI-84 Plus CE, written in C.
 ////////////////////////////////////////////////////////////////////////////////
 
-	static uint24_t numSprites;
-	static uint24_t sprites;
-  
+#define usb_callback_data_t usb_device_t
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <graphx.h>
+#include <keypadc.h>
+
+#include <srldrvce.h>
+#include <usbdrvce.h>
+
+#include "gfx/gfx.h"
+
+
+	static gfx_sprite_t *blocks[64];
+
 void main(void);
 void LoadBlocks(void);
+void DrawMenu(void);
+	
+	//ptr points to sprite data after the header
+	//len is length of sprite data
+	//sprites points to sprite pointers. Make sure it is big enough to hold the sprites.
+	int loadTextures(void *ptr, int len, gfx_sprite_t **sprites) {
+		int num = 0;
+		int offset = 0;
+			while (offset < len) {
+				sprites[num++] = ptr + offset;
+				offset += 16*16*2;
+			}
+			memcpy(blocks, sprites, sizeof(sprites));
+		return num;
+	}
+	
 
-//first, run LoadBlocks(), then, draw the main menu...
+//first, run LoadTextures(), then, draw the main menu...
 
 void main(void) {
+	ti_var_t appvar;
+    gfx_SetPalette(xlibc, sizeof_xlibc, 0);
+	//wherever you need to load the sprites
+	appvar = ti_Open("MCCESPR", "r");
+	loadTextures(ti_GetDataPtr(appvar)+2, ti_GetSize(appvar)-2, &blocks);
+
 	gfx_Begin();
-	gfx_SetPalette(myPalette, sizeof_myPalette, 0);
-  LoadBlocks();
-  DrawMenu();
-  
+	//gfx_SetPalette(myPalette, sizeof_myPalette, 0);
+
+  	DrawMenu();
+	
+	gfx_End();
 }
 
 void DrawMenu(void) {
 	int x, y, CursorY;
 	gfx_SetDrawBuffer();
-  for (x = 0; x < 20; x++) {
-  	for (y = 0; y < 15; y++) {
-    	gfx_SpriteNoClip(dirt, x * 16, y * 16);
-    }
-  }
-  
-	while !(kb_IsDown(kb_Key2nd)) {  
-  	kb_Scan();
-  	
-  	
-    gfx_BlitBuffer();
-  }
-  
-}
-
-
-void LoadBlocks(void) {
-	int x, spriteSize;
-  ti_var_t appvar;
-  
-	numSprites = 64;
-	spriteSize = (16 * 16) + 2;
-	appvar = ti_Open("pack", "r");
-	Pointer = ti_GetDataPtr(appvar);
-	ti_CloseAll();
-	data = numSprites * 3;
-	sprites = ti_mAlloc(N);
-	for (x = 0) {
-		sprites[x] = Pointer;
-		Pointer += spriteSize;
-		x += 3;
+	//somewhere global
+  	for (x = 0; x < 20; x++) {
+  		for (y = 0; y < 15; y++) {
+    		gfx_TransparentSprite_NoClip(blocks[1], x * 16, y * 16);
 		}
-
+  	}
+  
+	while (!(kb_IsDown(kb_Key2nd))) {  
+  		kb_Scan();
+  	
+  	
+    	gfx_BlitBuffer();
+  	}
+  
 }
 
