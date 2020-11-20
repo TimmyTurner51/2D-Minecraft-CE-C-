697566233cb6ceb4de3401c4815b1a3a16c7f565
Ryan <redryma@icloud.com>
2020-11-20 06:06:26 -0500

    Fixed main menu freeze bug
    Fixed a bug where when you exit from the play menu, everything else freezes and you can only enter and exit the play menu and is unable to exit the game.

--- src/main.c
+++ src/main.c
@@ -20,31 +20,41 @@
 #include <graphx.h>
 #include <keypadc.h>
 
-#include <srldrvce.h>
-#include <usbdrvce.h>
-
 #include "gfx/gfx.h"
 
-
+	bool submenu = false;
 	static ti_var_t appvar;
 	gfx_sprite_t *sprites[64];
 	gfx_sprite_t *ptr;
+
+	//define the world data list
+	static char WorldData[200 * 200] = { 0 };
+	
+	//{ name of world 1, name of world 2, etc. }
+	static char WorldsList[20] = { 0 };
+	//{ name of server 1, name of server 2, etc. }
+	static char Servers[20] = { 0 };
+	static char FriendsList[20] = { 0 };
 	
     gfx_TempSprite(logo, 16, 16);
 
 void main(void);
 void LoadBlocks(void);
 void DrawMenu(void);
-void Singleplayer(void);
-void Multiplayer(void);
+void PlayMenu(void);
+void Achievements(void);
 
 
 void main(void) {
 	gfx_Begin();
     gfx_SetPalette(xlibc, sizeof_xlibc, 0);
-	gfx_SetTransparentColor(255);
+	gfx_SetClipRegion(0-16, 0-16, 336, 256);
 	gfx_SetDrawBuffer();
 	LoadBlocks();
+	ti_CloseAll();
+	appvar = ti_Open("MC2DDAT", "r");
+	logo = ti_GetDataPtr(appvar);
+	ti_CloseAll();
   	DrawMenu();
 	
 	gfx_End();
@@ -53,70 +63,74 @@ void main(void) {
 }
 
 void DrawMenu(void) {
-	uint24_t CursorY, x, y, i, redraw, option, test;
-	ti_CloseAll();
-	appvar = ti_Open("MC2DDAT", "r");
-	logo = ti_GetDataPtr(appvar);
-	ti_CloseAll();
-	for (x = 0; x < 20; x++) {
-		for (y = 0; y < 15; y++) {
-			gfx_TransparentSprite_NoClip(sprites[1], x * 16, y * 16);
-		}
-	}
-	gfx_SetTextFGColor(230);
-	gfx_PrintStringXY("DEV_ALPHA v1.0.001", 3, 230);
-	gfx_SetTextFGColor(0);
-	gfx_ScaledTransparentSprite_NoClip(logo, 32, 10, 2, 2);
-    /* buttons */
-	gfx_SetColor(181);
-	gfx_FillRectangle(60, 140, 192, 16);
-	gfx_FillRectangle(60, 160, 192, 16);
-	/* to be used later when we update the main menu (to look "prettier" maybe)
-	gfx_FillRectangle(60, 180, 70, 16);
-	gfx_FillRectangle(182, 180, 70, 16);
-	*/
-	gfx_FillRectangle(60, 180, 192, 16);
-	gfx_FillRectangle(60, 200, 192, 16);
-	redraw = 1;
-	x = 60;
-	y = 140;
-	i = y;
-	while (!(kb_IsDown(kb_Key2nd))) {
-        gfx_SetColor(140);
+
+	uint24_t CursorY, x = 60, y = 125, i = y, option, test, scroll = 16, scrollY, redraw = 1, timer = 0;
+	gfx_SetTransparentColor(255);
+	while (!(kb_IsDown(kb_Key2nd)) && submenu == false) {
         kb_Scan();
-			if (redraw == 1) {
-				redraw = 0;
-				/* redraw only the one button that needs it */
-				gfx_SetColor(181);
-				gfx_FillRectangle(60, i, 192, 16);
-				/* button text */
-				gfx_PrintStringXY("Singleplayer", 119, 144);
-				gfx_PrintStringXY("Multiplayer", 122, 164);
-				gfx_PrintStringXY("Options", 132, 184);
-				gfx_PrintStringXY("Quit", 143, 204);
+		if (redraw == 1) {
+			for (test = 0; test < 20; test++) {
+				for (scrollY = scroll; scrollY < 256; scrollY += 16) {
+					gfx_TransparentSprite_NoClip(sprites[1], test * 16, scrollY - 16);
+				}
 			}
+			gfx_SetTextFGColor(230);
+			gfx_PrintStringXY("DEV_ALPHA v1.0.001", 3, 230);
+			gfx_SetTextFGColor(0);
+			gfx_ScaledTransparentSprite_NoClip(logo, 32, 20, 2, 2);
+			/* buttons */
+			gfx_SetColor(181);
+			gfx_FillRectangle(60, 125, 192, 20);
+			gfx_FillRectangle(60, 150, 192, 20);
+			gfx_FillRectangle(60, 175, 192, 20);
+			gfx_FillRectangle(60, 200, 192, 20);
+			gfx_SetColor(140);
+			/* redraw only the one button that needs it */
+			gfx_SetColor(181);
+			gfx_FillRectangle(60, i, 192, 20);
+			/* button text */
+			gfx_PrintStringXY("Play", 144, 130);
+			gfx_PrintStringXY("Achievements", 116, 155);
+			gfx_PrintStringXY("Settings", 130, 180);
+			gfx_PrintStringXY("Quit", 144, 205);
         i = y;
+		redraw = 0;
+	}
+		redraw = 1;
+		timer = 0;
+		scroll--;
+		if (scroll < 1) scroll = 16;
 		gfx_SetColor(0);
-		gfx_Rectangle(60, y, 192, 16);
-		gfx_Rectangle(61, y + 1, 190, 14);
+		gfx_Rectangle(60, y, 192, 20);
+		gfx_Rectangle(61, y + 1, 190, 18);
         gfx_BlitBuffer();
 		if (kb_IsDown(kb_KeyClear)) return;
-        if (kb_IsDown(kb_KeyUp) && y > 140) {
-            delay(150);
+        if (kb_IsDown(kb_KeyUp) && y > 125) {
+            //delay(150);
 			redraw = 1;
-            y -= 20;
+            y -= 25;
         }
         if (kb_IsDown(kb_KeyDown) && y < 200) {
-            delay(150);
+            //delay(150);
 			redraw = 1;
-            y += 20;
+            y += 25;
         }
         if (kb_IsDown(kb_Key2nd))                   option = 1;
+		gfx_BlitBuffer();
     }
-    if (y == 140) Singleplayer();
-    if (y == 160) Multiplayer();
-    if (y == 180) {
-			/* options */
+
+	gfx_SetTransparentColor(252);
+
+    if (y == 125) {
+		PlayMenu();
+		DrawMenu();
+	}
+    if (y == 150) {
+		Achievements();
+		DrawMenu();
+	}
+    if (y == 175) {
+			/* Game Settings */
 
 
 	}
@@ -125,19 +139,85 @@ void DrawMenu(void) {
   
 }
 
-void Singleplayer(void) {
+void PlayMenu(void) {
 
-	gfx_SetTransparentColor(252);
+	uint24_t tab, CursorY, x, y, i, redraw, option;
+	for (x = 0; x < 20; x++) {
+		for (y = 0; y < 15; y++) {
+			gfx_TransparentSprite_NoClip(sprites[1], x * 16, y * 16);
+		}
+	}
+	gfx_SetTransparentColor(255);
+	tab = 0;
+	CursorY = 40;
+	redraw = 1;
+	kb_Scan();
+	submenu = true;
+	while (!(kb_IsDown(kb_Key2nd)) && submenu == true) {
+		if (redraw == 1) {
+			gfx_SetColor(181);
+			gfx_FillRectangle(20, 20, 280, 200);
+			gfx_SetColor(0);
+			gfx_Rectangle(20, 20, 280, 200);
+			gfx_Rectangle(20, 20, 280, 20);
+			gfx_SetColor(148);
+			gfx_FillRectangle_NoClip(21 + (tab * 89), 21, 100, 18);
+			gfx_PrintStringXY("My Worlds", 40, 25);
+			gfx_PrintStringXY("Servers", 134, 25);
+			gfx_PrintStringXY("Friends", 240, 25);
+			redraw = 0;
+		}
 
+		kb_Scan();
+		if (kb_IsDown(kb_KeyLeft) && (tab > 0)) {
+			delay(200);
+			tab--;
+			redraw = 1;
+		}
+		if (kb_IsDown(kb_KeyRight) && (tab < 2)) {
+			delay(200);
+			tab++;
+			redraw = 1;
+		}
+		if (kb_IsDown(kb_KeyEnter)) {
+			submenu = false;
+			DrawMenu();
+		}
+
+		gfx_BlitBuffer();
+
+	}
+}
+
+void Achievements(void) {
+
+	uint24_t CursorY, x, y, i, redraw, option;
+	for (x = 0; x < 20; x++) {
+		for (y = 0; y < 15; y++) {
+			gfx_TransparentSprite_NoClip(sprites[1], x * 16, y * 16);
+		}
+	}
+	gfx_SetColor(181);
+	gfx_FillCircle_NoClip(10, 10, 5);
+	gfx_FillCircle_NoClip(309, 10, 5);
+	gfx_FillCircle_NoClip(10, 229, 5);
+	gfx_FillCircle_NoClip(309, 229, 5);
+	gfx_FillRectangle(10, 5, 300, 230);
+	gfx_FillRectangle(5, 10, 310, 220);
+	gfx_SetTextFGColor(0);
+	gfx_PrintStringXY("Achievements:", 20, 15);
+	gfx_SetTextFGColor(255);
+	for (x = 2; x < 18; x++) {
+		for (y = 2; y < 13; y++) {
+			gfx_TransparentSprite_NoClip(sprites[10], x * 16, y * 16);
+		}
+	}
+	
+	gfx_BlitBuffer();
+
+	while (!(os_GetCSC()));
 
 }
-void Multiplayer(void) {
-
-	gfx_SetTransparentColor(252);
-
-
-}
-
 
 void LoadBlocks(void) {
 	
@@ -157,4 +237,23 @@ void LoadBlocks(void) {
 			offset += 86;
 		}
 
+}
+
+void GenerateTerrain(void) {
+	int l = 0;
+	int h = 150;
+	int x = 0;
+	int y = 150;
+	int maxTilesX = 500;
+	int maxTilesY = -500;
+	os_ClrHome();
+	
+	for (x = 0; x <= maxTilesX; x+=16) {
+		l = x;
+		gfx_TransparentSprite_NoClip(sprites[1], x, y);
+		for (y = 150; y <= maxTilesY; y-=16) {
+			h = y;
+			gfx_TransparentSprite_NoClip(sprites[1], x, y);
+		}
+	}
 }
