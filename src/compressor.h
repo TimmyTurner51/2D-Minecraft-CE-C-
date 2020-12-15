0d3ee19c636f735f1e1c1a3f264c97e11d2af0d4
BeckATI on Mint 20 <beckadamtheartist@gmail.com>
2020-12-14 17:56:11 -0700

    tweak compressor.h

--- src/compressor.h
+++ src/compressor.h
@@ -2,8 +2,11 @@
 #ifndef __compressor_h__
 #define __compressor_h__
 
+//MAX_OFFSET and MAX_LEN may be lowered at the cost of compression ratio.
+//MAX_OFFSET is the maximum number of bytes that a pattern may be located in
+//MAX_LEN is the maximum pattern length, and has less effect on speed so should be left as-is.
 #define MAX_OFFSET  400 //2176  /* range 1..2176 */
-#define MAX_LEN    400 //65536  /* range 2..65536 */
+#define MAX_LEN    65536  /* range 2..65536 */
 
 
 int zx7_Compress(char *dest,char *src,int *len,int src_len);
