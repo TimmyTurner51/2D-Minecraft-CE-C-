3e6b3cdd3826e65f2b919247fe559d6be6ed4707
BeckATI on Mint 20 <beckadamtheartist@gmail.com>
2021-01-04 12:41:00 -0700

    Fix convimg.yaml

--- src/gfx/gfx.h
+++ src/gfx/gfx.h
@@ -1,31 +1,30 @@
-#ifndef gfx_include_file
-#define gfx_include_file
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "xlibc.h"
-#include "Head_1.h"
-#include "Head_2.h"
-#include "Head_3.h"
-#include "Head_4.h"
-#include "Head_Pumpkin.h"
-#include "Arm_1.h"
-#include "Arm_2.h"
-#include "Arm_3.h"
-#include "Arm_4.h"
-#include "Body_1.h"
-#include "Body_2.h"
-#include "Body_3.h"
-#include "Body_4.h"
-#include "Leg_1.h"
-#include "Leg_2.h"
-#include "Leg_3.h"
-#include "Leg_4.h"
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#ifndef gfx_include_file
+#define gfx_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Head_1.h"
+#include "Head_2.h"
+#include "Head_3.h"
+#include "Head_4.h"
+#include "Head_Pumpkin.h"
+#include "Arm_1.h"
+#include "Arm_2.h"
+#include "Arm_3.h"
+#include "Arm_4.h"
+#include "Body_1.h"
+#include "Body_2.h"
+#include "Body_3.h"
+#include "Body_4.h"
+#include "Leg_1.h"
+#include "Leg_2.h"
+#include "Leg_3.h"
+#include "Leg_4.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
